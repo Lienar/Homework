ff07156842148bac18e6d29be968e1be252f6715
Lienar <140402520+Lienar@users.noreply.github.com>
2024-04-14 13:08:48 +0300

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,11 @@
+car_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+
+car_count = 0
+
+for i in range (0, len(car_list)):
+    print (car_list[i])
+    car_count += 10
+
+print (" ")
+print (car_count)
+
