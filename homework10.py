768d704634f660b0a13fb922da6cb70953bc437d
Lienar <140402520+Lienar@users.noreply.github.com>
2024-04-23 11:00:28 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,13 @@
+def test():
+    a = 10
+    b = 21
+    print ('a = ', a, 'b = ', b)
+
+test()
+
+print (" ")
+
+def test2 (a, b, c):
+    print ('a = ', a, 'b = ', b, 'c = ', c)
+
+test2(20, 'abc', 35)
