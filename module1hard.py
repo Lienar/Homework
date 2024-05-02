48874357f89fc6715156ed28a096038226b33f6f
Lienar <140402520+Lienar@users.noreply.github.com>
2024-05-02 14:05:34 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -5,15 +5,13 @@ students_len = list(students)
 
 students_grades = {}
 students_len.sort()
-sum = 0
 
 for i in range (len(grades)):
-    for j in range (len(grades[i])):
-        sum = sum + int(grades[i][j])
-    students_grades[students_len[i]] = sum / len(grades[i])
-    sum = 0
+    students_grades[students_len[i]] = sum(grades[i]) / len(grades[i])
+
 
 for i in range (len(students_len)):
     print (students_len[i], ': ',students_grades[students_len[i]])
+
 
 
